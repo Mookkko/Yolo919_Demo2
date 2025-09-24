0b7f7510184b2c0874f42ef28a5e3c466272b387
Mookkko <mookko1421@gmail.com>
2025-09-24 15:27:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,7 @@ st.title("YOLO Image Detection App :)")
 # model = YOLO("runs/detect/train73/weights/best.pt")
 model = YOLO("best.pt")
 # Upload image
-uploaded_image = st.file_uploader("Upload an image (jpg, png)", type=["jpg", "
-jpeg", "png"])
-if uploaded_image is not None:
-# Show original image
+uploaded_image = st.file_uploader("Upload an image (jpg, png)", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
  # Show original image
